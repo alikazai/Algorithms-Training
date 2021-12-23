e76af224734749b18c45589c1730a5e59e9e213c
Ali Kazai <alik@digintus.com>
2021-12-23 23:24:33 +0000

    Created Queue Data Structure

---
+++ Queue/Model/MyQueue.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue.Model
+{
+    internal class MyQueue
+    {
+        public Node? First { get; set; }
+        public Node? Last { get; set; }
+        public int Length { get; set; }
+
+        public MyQueue()
+        {
+            First = null;
+            Last = null;
+            Length = 0;
+        }
+
+        public Node? Peek()
+        {
+            return First;
+        }
+
+        public Node Enqueue(string value)
+        {
+            Node? newNode = new Node(value);
+            if (IsEmpty())
+            {
+                First = newNode;
+                Last = newNode;
+            }
+            else
+            {
+                Last.Next = newNode;
+                Last= newNode;
+            }
+
+            Length++;
+            return newNode;
+        }
+
+        public Node? Dequeue()
+        {
+            if (First == null)
+                return null;
+
+            if(First == Last)
+                Last = null;
+
+            First = First.Next;
+            Length--;
+            return First;
+        }
+
+        private bool IsEmpty()
+        {
+            if (Length == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,26 @@
+﻿using Queue.Model;
+
+MyQueue myQueue = new();
+
+// ENQUEUE
+// Jon (0) - James (1) - Jack -(2)
+/*
+0 - Jon { value:"jon", next: "james"} //first
+1 - James { value:"james", next: "jack"}
+2 - Jack { value:"jack", next: "jane"}
+3 - Jane { value:"jane", next: null} //last
+*/
+myQueue.Enqueue("jon");
+myQueue.Enqueue("james");
+myQueue.Enqueue("jack");
+myQueue.Enqueue("jane");
+
+//DEQUEUE
+// Jon (0) - James (1) - --Jack -(2)--
+/*
+0 - James { value:"james", next: "jack"} //first
+1 - Jack { value:"jack", next: "jane"}
+2 - Jane { value:"jane", next: null} //last
+*/
+myQueue.Dequeue();
+myQueue.Peek();
