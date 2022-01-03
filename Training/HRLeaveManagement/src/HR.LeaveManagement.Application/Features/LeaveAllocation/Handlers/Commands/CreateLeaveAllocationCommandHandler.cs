0df48165335844025ee315ee0ea22497bef444c2
Ali Kazai <alik@digintus.com>
2022-01-03 01:07:51 +0000

    CQRS Add Commands

---
+++ Training/HRLeaveManagement/src/HR.LeaveManagement.Application/Features/LeaveAllocation/Handlers/Commands/CreateLeaveAllocationCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Requests.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Handlers.Commands
+{
+    public class CreateLeaveAllocationCommandHandler : IRequestHandler<CreateLeaveAllocationCommand, int>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public CreateLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> Handle(CreateLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = _mapper.Map<Domain.LeaveAllocation>(request.LeaveAllocationDto);
+            leaveAllocation = await _leaveAllocationRepository.Add(leaveAllocation);
+            return leaveAllocation.Id;
+        }
+    }
+}
---
+++ Training/HRLeaveManagement/src/HR.LeaveManagement.Application/Features/LeaveAllocation/Requests/Commands/CreateLeaveAllocationCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Application.DTOs.LeaveAllocation;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Requests.Commands
+{
+    public class CreateLeaveAllocationCommand : IRequest<int>
+    {
+        public CreateLeaveAllocationDto LeaveAllocationDto { get; set; }
+    }
+}
