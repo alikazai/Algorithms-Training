ef5dff57ef8a8b3aa1408c83ccf1281830c61d60
Ali Kazai <alik@digintus.com>
2022-01-07 04:07:48 +0000

    added migration and updated db

--- Training/HRLeaveManagement/HR.LeaveManagement.Api/Program.cs
+++ Training/HRLeaveManagement/HR.LeaveManagement.Api/Program.cs
@@ -1,11 +1,31 @@
+using HR.LeaveManagement.Application;
+using HR.LeaveManagement.Infrastructure;
+using HR.LeaveManagement.Persistence;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Custom Services
+builder.Services.ConfigureApplicationServices();
+builder.Services.ConfigureInfrastructureService(builder.Configuration);
+builder.Services.ConfigurePersistenceService(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo{Title = "HR.LeaveManagement.Api", Version = "v1"});
+});
+
+// Cors Policy
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CorsPolicy", b => 
+        b.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 
 var app = builder.Build();
 
@@ -19,6 +39,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
