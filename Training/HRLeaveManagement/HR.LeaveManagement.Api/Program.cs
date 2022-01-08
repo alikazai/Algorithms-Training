22953e7b836a873cbc4041460b8e6a2de4bb7203
Ali Kazai <alik@digintus.com>
2022-01-08 03:16:24 +0000

    updated api

--- Training/HRLeaveManagement/HR.LeaveManagement.Api/Program.cs
+++ Training/HRLeaveManagement/HR.LeaveManagement.Api/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo{Title = "HR.LeaveManagement.Api", Version = "v1"});
+    c.SwaggerDoc("v1", new OpenApiInfo{Title = "HR LeaveManagement Api", Version = "v1"});
 });
 
 // Cors Policy
