89d039067d1964f2d8600721e59657e9706d0727
Ali Kazai <alik@digintus.com>
2022-07-11 00:51:31 +0100

    added signin component as well as firebase utils and brought in firebase libs

--- Training/ReactUdemy/CapStone/ak-clothing/src/utils/firebase/firebase.utils.js
+++ Training/ReactUdemy/CapStone/ak-clothing/src/utils/firebase/firebase.utils.js
@@ -30,4 +30,17 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
 };
