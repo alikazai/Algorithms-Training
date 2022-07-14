424570c745b34d0548ac166cbd6136c3b38ac164
Ali Kazai <alik@digintus.com>
2022-07-14 02:13:34 +0100

    create authentication page component alongside sign in form

--- Training/ReactUdemy/CapStone/ak-clothing/src/routes/navigation/navigation.component.jsx
+++ Training/ReactUdemy/CapStone/ak-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,9 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
-import './navigation.styles.scss';
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
@@ -13,9 +13,15 @@ const Navigation = () => {
           <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link className="nav-link" to='/shop'>SHOP</Link>
-          <Link className="nav-link" to='/shop'>CONTACT</Link>
-          <Link className="nav-link" to='/sign-in'>SIGN IN</Link>
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/shop">
+            CONTACT
+          </Link>
+          <Link className="nav-link" to="/auth">
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
